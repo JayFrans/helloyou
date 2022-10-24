0ed8633085c764fc690c8b157b16832bb656ca08
JayFrans <114666891+JayFrans@users.noreply.github.com>
2022-10-24 15:02:15 +0200

    Add files via upload
    meeste code klaar, vooral nog verhaal en tekst aanpassen, misschien ook netter maken

--- eindopdracht.py
+++ eindopdracht.py
@@ -4,35 +4,84 @@ def Choices():
     print('temp')
     print('')
     print('')
-    print('')
-    print('')
 
-    answer1 = input()
+    answer = input()
 
-    if answer1 == 'A' or answer1 == 'a':
+    if answer == 'A' or answer == 'a':
         path1()
-    elif answer1 == 'B' or answer1 == 'b':
-        print('Persoonlijk niet de grootste fan van brood als ontbijt')
-    elif answer1 == 'C' or answer1 == 'c':
-        print('Ik zou tenminste wel iets eten')
-    else: print('Je kan alleen met A, B of C antwoorden')
-
-    print('Wil je nog een keer?')
-    print('Ja of Nee?')
-    retry = input()
-
-    if retry == 'Ja' or retry == 'ja':
-        Choices()
-    else: print('Goodbye!')
-Choices()
+    elif answer == 'B' or answer == 'b':
+        path2()
+    else: print('')
+Choices() 
 
 def path1():
+    print('')
+    print('')
+
     answer1 = input()
 
     if answer1 == 'A' or answer1 == 'a':
-        path1()
+        path11()
     elif answer1 == 'B' or answer1 == 'b':
-        print('Persoonlijk niet de grootste fan van brood als ontbijt')
-    elif answer1 == 'C' or answer1 == 'c':
-        print('Ik zou tenminste wel iets eten')
-    else: print('Je kan alleen met A, B of C antwoorden')
+        path12
+    else: print('')
+
+def path2():
+    print('')
+    print('')
+
+    answer1 = input()
+
+    if answer1 == 'A' or answer1 == 'a':
+        path21()
+    elif answer1 == 'B' or answer1 == 'b':
+        path22()
+    else: print('')
+    
+def path11():
+    print('')
+    print('')
+
+    answer1 = input()
+
+    if answer1 == 'A' or answer1 == 'a':
+        'temp'
+    elif answer1 == 'B' or answer1 == 'b':
+        'temp'
+    else: print('')
+
+def path12():
+    print('')
+    print('')
+
+    answer1 = input()
+
+    if answer1 == 'A' or answer1 == 'a':
+        'temp'
+    elif answer1 == 'B' or answer1 == 'b':
+        'temp'
+    else: print('')
+
+def path21():
+    print('')
+    print('')
+
+    answer1 = input()
+
+    if answer1 == 'A' or answer1 == 'a':
+        'temp'
+    elif answer1 == 'B' or answer1 == 'b':
+        'temp'
+    else: print('')
+
+def path22():
+    print('')
+    print('')
+
+    answer1 = input()
+
+    if answer1 == 'A' or answer1 == 'a':
+        'temp'
+    elif answer1 == 'B' or answer1 == 'b':
+        'temp'
+    else: print('')
